3cce4febdcaab4841e510453ccf90e65d81ffd42
Alejandra Victoria <ale.victoria.alcaraz@gmail.com>
2019-06-06 00:36:39 -0500

    Update matrix.F90

--- matrix/matrix.F90
+++ matrix/matrix.F90
@@ -1,23 +1,22 @@
 program Matrix
 
-real, dimension(3, 3) :: mat !two dimensional real array
-integer :: i, j
-
-! assigning some values to the matrix
-!do i = 1, 3
-!	do j = 1, 3
-!		matrix(i, j) = i * j
-!	end do
-!end do
+real(kind = 16), dimension(3, 3) :: mat !two dimensional real array
+integer :: i, j, x
 
 ! reading matrix from file
 open(1, file='matrix_data.dat')
 do i = 1, 3
 	do j = 1, 3
+		!read(1, *, err=30, iostat=x) mat(i, j)
 		read(1, *) mat(i, j)
 	end do
 end do
+
+!30 print *, 'Not a valid number in the file: ', x
+!	stop
+
 close(1)
+
 
 ! print matrix
 print *, 'Initial Matrix'
