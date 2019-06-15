847efd5dce0f375d62f7eebb2e39e6e9ebce5685
Alejandra Victoria <ale.victoria.alcaraz@gmail.com>
2019-06-15 03:12:17 -0500

    Update infix_converter.py

--- infix_converter.py
+++ infix_converter.py
@@ -59,4 +59,41 @@ def infix_to_prefix(expression):
 	result = invert_string(result)
 	print ("Prefix: %s" % result)
 	
+def infix_to_postfix(expression):
+	result = ''
+	stack = []
+	print ("Infix: %s" % expression)
+	for c in expression:
+		if c == '(':
+			stack.append(c)
+		elif c == ')':
+			for i in range(0, len(stack)):
+				top = stack.pop()
+				if top != '(':
+					result += top
+				else:
+					break
+		elif c not in operators_keys:
+			result += c
+		elif c in operators_keys:
+			if len(stack) > 0:
+				val = operators[c]
+				for i in range(0, len(stack)):
+					top = stack.pop()
+					if operators[top] >= val: # top has higher precedence
+						result += top
+						if(len(stack) == 0):
+							stack.append(c)
+					else:
+						stack.append(top) # return back value to stack
+						stack.append(c)
+						break
+			else:
+				stack.append(c)
+	for i in range(0, len(stack)):
+		top = stack.pop()
+		result += top
+	print ("Postfix: %s" % result)
+	
 infix_to_prefix("(A+B^C)*D+E^5+8-7")
+infix_to_postfix("(A+B^C)*D+E^5+8-7")
